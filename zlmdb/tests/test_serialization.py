428985778b543abbb90559c02f67156d963a1bec
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-06-12 19:48:49 +0200

    serialization speed tests

---
+++ zlmdb/tests/test_serialization.py
@@ -0,0 +1,86 @@
+import sys
+import timeit
+import random
+import uuid
+import datetime
+
+import humanize
+
+from zlmdb._pmap import _JsonValuesMixin, _CborValuesMixin, _PickleValuesMixin
+
+if sys.version_info >= (3, 6):
+    from user_typed import User
+else:
+    from user import User
+
+
+def _create_test_user():
+    user = User()
+    user.oid = random.randint(0, 2**64-1)
+    user.name = 'Test {}'.format(user.oid)
+    user.authid = 'test-{}'.format(user.oid)
+    user.uuid = uuid.uuid4()
+    user.email = '{}@example.com'.format(user.authid)
+    user.birthday = datetime.date(1950, 12, 24)
+    user.is_friendly = True
+    user.tags = ['relaxed', 'beerfan']
+    for j in range(10):
+        user.ratings['test-rating-{}'.format(j)] = random.random()
+    return user
+
+
+def _serialization_speed(serializer):
+    total = {
+        'bytes': 0
+    }
+
+    def test():
+        user = _create_test_user()
+        data = serializer._serialize_value(user.marshal())
+        total['bytes'] += len(data)
+
+    N = 20
+    M = 10000
+
+    samples = []
+
+    print('')
+    for i in range(N):
+        secs = timeit.timeit(test, number=M)
+        ops = round(float(M) / secs, 1)
+        samples.append(ops)
+        print('{} objects/sec {}'.format(ops, humanize.naturalsize(total['bytes'])))
+
+    ops_max = max(samples)
+    print('{} objects/sec max, {} bytes total'.format(ops_max, humanize.naturalsize(total['bytes'])))
+
+    return ops_max, total['bytes']
+
+
+def test_json_serialization_speed():
+    serializer = _JsonValuesMixin()
+    ops_max, total = _serialization_speed(serializer)
+    # cpy36: 19564.6 objects/sec max, 135456153 bytes total
+    assert ops_max > 1000
+    assert total > 1000000
+
+
+def test_cbor_serialization_speed():
+    serializer = _CborValuesMixin()
+    ops_max, total = _serialization_speed(serializer)
+    # cpy36: 7787.4 objects/sec max, 97815364 bytes total
+    assert ops_max > 1000
+    assert total > 1000000
+
+
+def test_pickle_serialization_speed():
+    serializer = _PickleValuesMixin()
+    ops_max, total = _serialization_speed(serializer)
+    # cpy36: 33586.0 objects/sec max, 137738869 bytes total
+    assert ops_max > 1000
+    assert total > 1000000
+
+
+if __name__ == '__main__':
+    for serializer in [_JsonValuesMixin(), _CborValuesMixin(), _PickleValuesMixin()]:
+        print(_serialization_speed(serializer))
