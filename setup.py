8ff5a0b36d50025fd93a928af6e21a4643d99dc5
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-09-11 16:24:01 +0200

    update lib

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="Object-relational zero-copy in-memory database layer for LMDB.",
     entry_points={
