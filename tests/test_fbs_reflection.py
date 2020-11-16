c5202f3fc9356570b8f68d549525bdb8f8f04934
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-11-16 13:19:39 +0100

    sync

--- tests/test_fbs_reflection.py
+++ tests/test_fbs_reflection.py
@@ -3,5 +3,9 @@ txaio.use_twisted()
 
 from autobahn.xbr._schema import FbsSchema
 
-schema = FbsSchema.load('/tmp/test/bfbs/climate.bfbs')
-print(schema)
+for filename in [
+    '/tmp/test/bfbs/climate.bfbs',
+    '/tmp/test/bfbs/network.bfbs',
+    '/tmp/test/bfbs/location.bfbs']:
+    schema = FbsSchema.load(filename)
+    print(schema)
