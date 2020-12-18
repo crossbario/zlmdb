5c3c212e70d1f025208543157af4691086bf96b9
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-12-18 20:24:28 +0100

    try fix (#33)
    fix github actions ci configuration

--- deploy.sh
+++ deploy.sh
@@ -2,38 +2,13 @@
 
 set +o verbose -o errexit
 
-# AUTOBAHN_VERSION             : must be set in travis.yml!
 export AWS_DEFAULT_REGION=eu-central-1
 export AWS_S3_BUCKET_NAME=crossbarbuilder
-# AWS_ACCESS_KEY_ID         : must be set in Travis CI build context!
-# AWS_SECRET_ACCESS_KEY     : must be set in Travis CI build context!
-# WAMP_PRIVATE_KEY          : must be set in Travis CI build context!
-
-# TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, TRAVIS_TAG
-
-# PR => don't deploy and exit
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-    echo '[1] deploy script called for PR - exiting ..';
-    exit 0;
-
-# direct push to master => deploy
-elif [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    echo '[2] deploy script called for direct push to master: continuing to deploy!';
-
-# tagged release => deploy
-elif [ -n "$TRAVIS_TAG" ]; then
-    echo '[3] deploy script called for tagged release: continuing to deploy!';
-
-# outside travis? => deploy
-else
-    echo '[?] WARNING: deploy script called outside Travis? will not deploy!';
-    exit 0;
-
-fi
+# AWS_ACCESS_KEY_ID         : must be set in CI build context!
+# AWS_SECRET_ACCESS_KEY     : must be set in CI build context!
+# WAMP_PRIVATE_KEY          : must be set i CI build context!
 
 # only show number of env vars .. should be 4 on master branch!
-# https://docs.travis-ci.com/user/pull-requests/#Pull-Requests-and-Security-Restrictions
-# Travis CI makes encrypted variables and data available only to pull requests coming from the same repository.
 echo 'aws env vars (should be 4 - but only on master branch!):'
 env | grep AWS | wc -l
 
